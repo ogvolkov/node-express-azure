b69dd14fc8518c89c60a493327181777ae15292c
Oleg Volkov <OGVolkov@gmail.com>
2019-04-17 00:48:27 +0200

    deployment name

--- deploy-resources.cmd
+++ deploy-resources.cmd
@@ -1,4 +1,5 @@
 az group deployment create ^
     --resource-group node-express-azure-example ^
     --template-file azuredeploy.json ^
-    --parameters site_host_name=node-express-azure-example app_svc_plan_name=node-express-azure-example-plan
+    --parameters site_host_name=node-express-azure-example app_svc_plan_name=node-express-azure-example-plan ^
+    --name node-express-azure-example
